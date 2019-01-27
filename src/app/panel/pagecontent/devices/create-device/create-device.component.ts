f0888bd9ac852efb2d822c44e382508a923458b8
alizohrevand <alihooshyar1990@gmail.com>
2019-01-27 13:50:49 +0330

    base update device html added

--- src/app/panel/pagecontent/devices/create-device/create-device.component.ts
+++ src/app/panel/pagecontent/devices/create-device/create-device.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Form, NgForm} from '@angular/forms';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-create-device',
@@ -7,18 +7,18 @@ import {Form, NgForm} from '@angular/forms';
   styleUrls: ['./create-device.component.css']
 })
 export class CreateDeviceComponent implements OnInit {
-  section = 1;
-  totalSection = 6;
-  advanceConfig = false;
-  deviceName: string;
-  deviceOwnerName: string;
-  deviceKey: string;
-  location: any;
-  password: any;
-  rePassword: any;
-  publishTopicName: string;
-  basePathToAddTopicAddress: string;
-  ArrayPublishTopicAddress: string[] ;
+  private section = 1;
+  private totalSection = 6;
+  private advanceConfig = false;
+  private deviceName: string;
+  private deviceOwnerName: string;
+  private deviceKey: string;
+  private location: any;
+  private password: any;
+  private rePassword: any;
+  private publishTopicName: string;
+  private basePathToAddTopicAddress: string;
+  private ArrayPublishTopicAddress: string[];
 
   constructor() {
     this.deviceOwnerName = 'GetdeviceOwnerName';
--- src/app/panel/pagecontent/devices/update-device/update-device.component.ts
+++ src/app/panel/pagecontent/devices/update-device/update-device.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-update-device',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-device.component.css']
 })
 export class UpdateDeviceComponent implements OnInit {
+  private advanceConfig = false;
+  private deviceName: string;
+  private deviceOwnerName: string;
+  private deviceKey: string;
+  private location: any;
+  private password: any;
+  private rePassword: any;
+  private publishTopicName: string;
+  private basePathToAddTopicAddress: string;
+  private ArrayPublishTopicAddress: string[];
 
-  constructor() { }
+  constructor() {
+    this.deviceOwnerName = 'GetdeviceOwnerName';
+    this.deviceKey = 'GetdeviceKey';
+    this.basePathToAddTopicAddress = '/serverId/UserName';
+    this.ArrayPublishTopcAddress = [];
+  }
 
   ngOnInit() {
   }
 
+  addPublishTopicAdress() {
+    this.ArrayPublishTopicAddress.push(this.publishTopicName);
+  }
+
+  deletPublishTopic(i: number) {
+    if (i > -1 && i < this.ArrayPublishTopicAddress.length) {
+      this.ArrayPublishTopicAddress.splice(i, 1);
+    }
+  }
 }
