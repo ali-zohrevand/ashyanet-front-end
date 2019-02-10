448d2d9f5ee45b4e9d1465c7eaee321306f6b2ff
gabriel <gabriel@it.sh>
2019-02-10 17:36:04 +0330

    list device added

--- src/app/panel/pagecontent/main/main.component.ts
+++ src/app/panel/pagecontent/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../../../services/API/api.service';
 import {Location} from '../../../models/Locations/location';
 import {Device} from '../../../models/device/device';
+import {DevicesService} from '../../../models/device/devices.service';
 
 @Component({
   selector: 'app-main',
@@ -11,16 +12,11 @@ import {Device} from '../../../models/device/device';
 export class MainComponent implements OnInit {
   deviceNumber: number;
   LocaionNumber: number;
-  constructor(private apiServices: ApiService) { this.deviceNumber = 0; this.LocaionNumber=0; }
+  constructor(private apiServices: DevicesService,private APiServices: ApiService) { this.deviceNumber = 0; this.LocaionNumber=0; }
 
   ngOnInit() {
-    this.apiServices.getApi('devices').subscribe(
-      ((reponse: Device[]) => {
-        this.deviceNumber = reponse.length;
-      }), (error: Response) => {
-      }
-    );
-    this.apiServices.getApi('location').subscribe(
+   this.deviceNumber= this.apiServices.getDevices().length;
+    this.APiServices.getApi('location').subscribe(
       (locations: Location[]) => {
         this.LocaionNumber = locations.length;
       },
