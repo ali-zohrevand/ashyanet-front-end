c9bfe00bec43b411ca77501f887c87adf0a2a737
gabriel <gabriel@it.sh>
2019-02-12 15:59:24 +0330

    create location form basic

--- src/app/panel/pagecontent/types/type-create/type-create.component.ts
+++ src/app/panel/pagecontent/types/type-create/type-create.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Types} from '../../../../models/Types/types';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-type-create',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./type-create.component.css']
 })
 export class TypeCreateComponent implements OnInit {
+  typeObj: Types;
+  @ViewChild('createForm') createForm: NgForm;
 
   constructor() { }
 
